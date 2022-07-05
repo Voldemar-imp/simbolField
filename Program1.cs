70b5c714c741e441daafa1d39ffca4d34fc3500d
Voldemar-imp <108013210+Voldemar-imp@users.noreply.github.com>
2022-07-05 20:13:18 +0300

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            char simbol;
+            int stringLength;
+            string simbolField = ""; 
+
+            Console.WriteLine("Введите слово: ");
+            userInput = Console.ReadLine();
+            stringLength = userInput.Length;
+            Console.WriteLine("Введите символ: ");
+            simbol = Convert.ToChar(Console.ReadLine());
+            
+            for (int i = 0; i < stringLength; i++)
+            {
+                simbolField += simbol; 
+            }
+            Console.Clear ();
+            Console.WriteLine("{0}{1}{0}", simbol, simbolField);
+            Console.WriteLine("{0}{1}{0}", simbol, userInput);
+            Console.WriteLine("{0}{1}{0}", simbol, simbolField);
+
+        }
+    }
+}
